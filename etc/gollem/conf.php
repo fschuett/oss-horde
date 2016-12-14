2632c7bdc309294437ee0ecd96bc1b93ea1a56f9
Frank Schütte <fschuett@gymhim.de>
2016-12-14 11:47:34 +0100

    fix some config settings

--- etc/gollem/conf.php
+++ etc/gollem/conf.php
@@ -1,9 +1,10 @@
 <?php
 /* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
-// $Horde: gollem/config/conf.xml,v 1.5.2.2 2005/12/11 18:31:18 slusarz Exp $
+// $Id: c70cc328a58f2b69cb67558ab883380298313e1e $
 $conf['manager']['date_format'] = '%x';
 $conf['backend']['backend_list'] = 'hidden';
 $conf['menu']['apps'] = array();
 $conf['user']['alternate_login'] = false;
 $conf['user']['redirect_on_logout'] = false;
+$conf['foldercache']['use_cache'] = false;
 /* CONFIG END. DO NOT CHANGE ANYTHING IN OR BEFORE THIS LINE. */
--- etc/turba/conf.php
+++ etc/turba/conf.php
@@ -1,10 +1,10 @@
 <?php
 /* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
-// $Horde: turba/config/conf.xml,v 1.6.2.5 2008/05/06 21:26:59 bklang Exp $
+// $Id: 4cd616848fb2e5c81200bf7c65930e9086ec2dcd $
 $conf['menu']['import_export'] = true;
-$conf['menu']['apps'] = array();
 $conf['client']['addressbook'] = 'localsql';
 $conf['shares']['source'] = 'localsql';
 $conf['comments']['allow'] = true;
 $conf['documents']['type'] = 'horde';
+$conf['tags']['enabled'] = true;
 /* CONFIG END. DO NOT CHANGE ANYTHING IN OR BEFORE THIS LINE. */
